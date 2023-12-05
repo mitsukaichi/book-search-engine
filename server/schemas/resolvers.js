f344450da87e506032ceb42171e4657a4cfa8b63
Minami Itsukaichi <mitukaichi@gmail.com>
2023-12-04 17:06:15 -0800

    add typedefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,49 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers= {
+    Query: {
+        me:  async (parent, { username }) => {
+            return User.findOne({ username: username }).populate('savedBooks');
+          },
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });     
+            if (!user) {
+                throw AuthenticationError;
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { bookToSave, username }) => {
+            return User.findOneAndUpdate(
+                { username: username },
+                { $addToSet: { savedBooks: {bookToSave} }},
+                { new: true, }
+        )},
+        removeBook: async (parent, { bookId, username }) => {
+            return User.findOneAndUpdate(
+                { username: username},
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
+            )
+        }
+
+    }
+
+
+
+
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Books]!
+    bookCount: Int
+  }
+
+  type Books {
+    bookId: ID
+    description: String
+    image: String
+    link: String
+    title: String
+    authors: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me(username: String!): User
+  }
+
+  input BookData {
+    bookId: ID
+    description: String
+    image: String
+    link: String
+    title: String
+    authors: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookToSave: BookData!, username: String!): User
+    removeBook(bookId: ID!, username: String!): User
+  }
+`;
+
+module.exports = typeDefs;
